9f6ae098d97a17b900d4a801137b0794a1f0229c
Christos Karamoustos <christos.karamoustos@gmail.com>
2017-04-20 00:05:43 +0300

    Add files via upload

---
+++ Reminder/Reminder/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace Reminder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(13, 40);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(236, 111);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.Enter += new System.EventHandler(this.textBox1_Enter);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(256, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(73, 40);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "New";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(256, 186);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(73, 40);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Edit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(12, 12);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(238, 22);
+            this.dateTimePicker1.TabIndex = 0;
+            this.dateTimePicker1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.dateTimePicker1_KeyDown);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 186);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(237, 82);
+            this.listBox1.TabIndex = 4;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            this.listBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.listBox1_KeyDown);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(53, 157);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(174, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 161);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Name";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Reminder is running";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(13, 277);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(153, 17);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Run this program at startup";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Myriad Hebrew", 6.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(172, 276);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(44, 20);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Apply";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(341, 301);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Reminder";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Reminder/Reminder/Form1.cs
@@ -0,0 +1,271 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Reminder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            RegistryKey rk = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run\\Reminder", false);
+            if (rk == null)
+                checkBox1.Checked = false;
+            else
+                checkBox1.Checked = true;
+
+            string[] binFilesInDir = Directory.GetFiles(Directory.GetCurrentDirectory(), "*.bin");
+            foreach (string file in binFilesInDir)
+                listBox1.Items.Add(Path.GetFileNameWithoutExtension(file));
+            notifyIcon1.Visible = true;
+            //int fileNo = 0;
+            //string fileName = "reminder" + fileNo + ".bin";
+            //while (File.Exists(Directory.GetCurrentDirectory() + "\\" + fileName))
+            //{
+            //    fileNo++;
+            //    fileName = "reminder" + fileNo + ".bin";
+            //}
+            //textBox2.Text = Path.GetFileNameWithoutExtension(fileName);
+
+            button1.Enabled = true;
+            button2.Enabled = true;
+
+            dateTimePicker1.Format = DateTimePickerFormat.Custom;
+            dateTimePicker1.CustomFormat = "dd - MMM - yyyy,  HH : mm";
+            DateTime dt = dateTimePicker1.Value;
+            dateTimePicker1.Value = new DateTime(dt.Year, dt.Month, dt.Day, dt.Hour, dt.Minute, 0);
+            timer1.Interval = 60000;
+            timer1.Start();
+        }
+
+        private void dateTimePicker1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                this.SelectNextControl(this.dateTimePicker1, true, true, true, true);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "New")
+            {
+                int fileNo = 0;
+                string fileName = "reminder" + fileNo + ".bin";
+                while (File.Exists(Directory.GetCurrentDirectory() + "\\" + fileName))
+                {
+                    fileNo++;
+                    fileName = "reminder" + fileNo + ".bin";
+                }
+                textBox2.Text = Path.GetFileNameWithoutExtension(fileName);
+                textBox2.Focus();
+                button1.Text = "Save";
+                button1.Enabled = true;
+                button2.Enabled = false;
+                listBox1.Enabled = false;
+                //timer1.Stop();
+                //Formatter f = new Formatter();
+                //f.dateAndTimePick = dateTimePicker1.Value;
+                //f.text = textBox1.Text;
+                //f.enabled = true;
+                //IFormatter formatter = new BinaryFormatter();
+                //Stream stream = new FileStream(fileName, FileMode.Create, FileAccess.Write, FileShare.None);
+                //formatter.Serialize(stream, f);
+                //stream.Close();
+                //timer1.Start();
+                ////MessageBox.Show("Reminder Created");
+                //textBox1.Text = "";
+                //listBox1.Items.Add(Path.GetFileNameWithoutExtension(fileName));
+
+            }
+            else if (button1.Text == "Save")
+            {
+                if (textBox2.Text != "" && textBox1.Text != "")
+                {
+                    string file = Directory.GetCurrentDirectory() + "\\" + textBox2.Text + ".bin";
+                    if (File.Exists(file))
+                    {
+                        if (MessageBox.Show("You are going to overwite the file.", "Are you sure?", MessageBoxButtons.OKCancel) == System.Windows.Forms.DialogResult.Cancel)
+                        {
+                            textBox2.Focus();
+                            textBox1.Text = "";
+                            button1.Text = "New";
+                            listBox1.Enabled = true;
+                            button1.Enabled = true;
+                            button2.Enabled = true;
+                            return;
+                        }
+                    }
+                    timer1.Stop();
+                    Formatter f = new Formatter();
+                    f.dateAndTimePick = dateTimePicker1.Value;
+                    f.text = textBox1.Text;
+                    f.enabled = true;
+                    IFormatter formatter = new BinaryFormatter();
+                    Stream stream = new FileStream(file, FileMode.Create, FileAccess.Write, FileShare.None);
+                    formatter.Serialize(stream, f);
+                    stream.Close();
+                    timer1.Start();
+                    textBox1.Text = "";
+                    button1.Text = "New";
+                    listBox1.Enabled = true;
+                    button1.Enabled = true;
+                    button2.Enabled = true;
+
+                    listBox1.Items.Clear();
+                    string[] binFilesInDir = Directory.GetFiles(Directory.GetCurrentDirectory(), "*.bin");
+                    foreach (string fileN in binFilesInDir)
+                        listBox1.Items.Add(Path.GetFileNameWithoutExtension(fileN));
+                    //MessageBox.Show("Done");
+                }
+                else
+                {
+                    if (textBox2.Text == "")
+                    {
+                        MessageBox.Show("You can not save to emty file");
+                        textBox2.Focus();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Please type some text");
+                        textBox1.Focus();
+                    }
+                }  
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+            button1.Enabled = true;
+            listBox1.Enabled = false;
+            button1.Text = "Save";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string[] binFilesInDir = Directory.GetFiles(Directory.GetCurrentDirectory(), "*.bin");
+            foreach (string file in binFilesInDir)
+            {
+                IFormatter formatter1 = new BinaryFormatter();
+                Stream stream1 = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.Read);
+                Formatter f1 = (Formatter)formatter1.Deserialize(stream1);
+                stream1.Close();
+                if (f1.enabled && f1.dateAndTimePick <= DateTime.Now)
+                {
+                    // write back to the file for assign the false value to enabled
+                    f1.enabled = false;
+                    IFormatter formatter = new BinaryFormatter();
+                    Stream stream = new FileStream(file, FileMode.Create, FileAccess.Write, FileShare.None);
+                    formatter.Serialize(stream, f1);
+                    stream.Close();
+                    f1.text = f1.dateAndTimePick.ToString() + "\n\n" + f1.text;
+                    MessageBox.Show(f1.text, Path.GetFileNameWithoutExtension(file));
+                }
+            }
+        }
+
+        [Serializable]
+        public class Formatter
+        {
+            public DateTime dateAndTimePick;
+            public string text;
+            public bool enabled;
+        }
+
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox2.Text = listBox1.Text;
+            string file = Directory.GetCurrentDirectory() + "\\" + listBox1.Text + ".bin";
+            if (File.Exists(file))
+            {
+                IFormatter formatter1 = new BinaryFormatter();
+                Stream stream1 = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.Read);
+                Formatter f1 = (Formatter)formatter1.Deserialize(stream1);
+                stream1.Close();
+                textBox1.Text = f1.text;
+                dateTimePicker1.Value = f1.dateAndTimePick;
+            }
+        }
+
+        private void listBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Delete && MessageBox.Show("Delete the selected reminder?", "Are you sure?", MessageBoxButtons.YesNo) == System.Windows.Forms.DialogResult.Yes)
+            {
+                string file = Directory.GetCurrentDirectory() + "\\" + listBox1.Text + ".bin";
+                if (File.Exists(file))
+                {
+                    File.Delete(file);
+                    string[] binFilesInDir = Directory.GetFiles(Directory.GetCurrentDirectory(), "*.bin");
+                    listBox1.Items.Clear();
+                    foreach (string f in binFilesInDir)
+                        listBox1.Items.Add(Path.GetFileNameWithoutExtension(f));
+                }
+                else
+                    MessageBox.Show("File not found");
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //if (MessageBox.Show("Closing this application will stop all reminders, continue?", "Closing", MessageBoxButtons.YesNo) == System.Windows.Forms.DialogResult.No)
+            //    e.Cancel = true;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (FormWindowState.Minimized == this.WindowState)
+            {
+                this.Hide();
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            RegistryKey rk = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            if (checkBox1.Checked)
+            {
+                rk.SetValue("Reminder", Application.ExecutablePath.ToString());
+                MessageBox.Show("Enabled");
+            }
+            else
+            {
+                rk.DeleteValue("Reminder", false);
+                MessageBox.Show("Disabled");
+            }
+        }
+    }
+}
